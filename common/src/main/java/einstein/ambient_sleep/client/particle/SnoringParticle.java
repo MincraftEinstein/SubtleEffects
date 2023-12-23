4b69f6e02ea77a729b77d45969bbea3f3b3d8186
MincraftEinstein <70532030+MincraftEinstein@users.noreply.github.com>
2023-12-23 18:19:44 -0500

    Update SnoringParticle.java

--- common/src/main/java/einstein/ambient_sleep/client/particle/SnoringParticle.java
+++ common/src/main/java/einstein/ambient_sleep/client/particle/SnoringParticle.java
@@ -10,18 +10,18 @@ import org.jetbrains.annotations.Nullable;
 
 public class SnoringParticle extends SmokeParticle {
 
-    protected SnoringParticle(ClientLevel level, double x, double y, double z, double xSpeed, double ySpeed, double zSpeed, SpriteSet spriteSet) {
-        super(level, x, y, z, xSpeed, ySpeed, zSpeed, 1, spriteSet);
+    protected SnoringParticle(ClientLevel level, double x, double y, double z, double xSpeed, double ySpeed, double zSpeed, SpriteSet sprites) {
+        super(level, x, y, z, xSpeed, ySpeed, zSpeed, 1, sprites);
         setColor(1, 1, 1);
         setLifetime(20);
     }
 
-    public record Provider(SpriteSet spriteSet) implements ParticleProvider<SimpleParticleType> {
+    public record Provider(SpriteSet sprites) implements ParticleProvider<SimpleParticleType> {
 
         @Nullable
         @Override
         public Particle createParticle(SimpleParticleType type, ClientLevel level, double x, double y, double z, double xSpeed, double ySpeed, double zSpeed) {
-            return new SnoringParticle(level, x, y, z, xSpeed, ySpeed, zSpeed, spriteSet);
+            return new SnoringParticle(level, x, y, z, xSpeed, ySpeed, zSpeed, sprites);
         }
     }
 }
