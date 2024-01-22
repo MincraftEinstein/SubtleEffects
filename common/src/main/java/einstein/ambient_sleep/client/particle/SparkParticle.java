ba48ef99317e8a60220fe4b55f4f18400056c393
MincraftEinstein <70532030+MincraftEinstein@users.noreply.github.com>
2024-01-22 14:12:34 -0500

    Reworked spark particle

--- common/src/main/java/einstein/ambient_sleep/client/particle/SparkParticle.java
+++ common/src/main/java/einstein/ambient_sleep/client/particle/SparkParticle.java
@@ -1,19 +1,38 @@
 package einstein.ambient_sleep.client.particle;
 
 import net.minecraft.client.multiplayer.ClientLevel;
-import net.minecraft.client.particle.BaseAshSmokeParticle;
-import net.minecraft.client.particle.Particle;
-import net.minecraft.client.particle.ParticleProvider;
-import net.minecraft.client.particle.SpriteSet;
+import net.minecraft.client.particle.*;
 import net.minecraft.core.particles.SimpleParticleType;
+import net.minecraft.util.Mth;
 import org.jetbrains.annotations.Nullable;
 
-public class SparkParticle extends BaseAshSmokeParticle {
+public class SparkParticle extends TextureSheetParticle {
 
     protected SparkParticle(ClientLevel level, double x, double y, double z, double xSpeed, double ySpeed, double zSpeed, float lifeTimeModifier, SpriteSet sprites) {
-        super(level, x, y, z, 0.1F, 0.1F, 0.1F, xSpeed, ySpeed, zSpeed, level.random.nextInt(11) / lifeTimeModifier, sprites, 0, 20, -0.1F, true);
-        quadSize = 0.1F * (random.nextFloat() * 0.5F + 0.5F) * 2.0F * 0.75F * random.nextInt(11) / 10;
+        super(level, x, y, z, xSpeed, ySpeed, zSpeed);
+        friction = 0.96F;
+        gravity = -0.1F;
+        xd = (xd * 0.1) + xSpeed;
+        yd = (yd * 0.1) + ySpeed;
+        zd = (yd * 0.1) + zSpeed;
+        int i = random.nextInt(11);
+        quadSize *= 0.75F * i / 10;
+        lifetime = (int) (20 / (random.nextFloat() * 0.8 + 0.2) * i / lifeTimeModifier);
+        lifetime = Math.max(lifetime, 1);
+        hasPhysics = true;
+        speedUpWhenYMotionIsBlocked = true;
         setColor(1, 1, 1);
+        pickSprite(sprites);
+    }
+
+    @Override
+    public ParticleRenderType getRenderType() {
+        return ParticleRenderType.PARTICLE_SHEET_OPAQUE;
+    }
+
+    @Override
+    public float getQuadSize(float scaleFactor) {
+        return quadSize * Mth.clamp((age + scaleFactor) / lifetime * 32.0F, 0.0F, 1.0F);
     }
 
     @Override
