2fcd5910f1e68fa344ae2343e21fc546e6fa5948
MincraftEinstein <70532030+MincraftEinstein@users.noreply.github.com>
2024-03-23 15:08:30 -0400

    Redid firefly particle (works much better now)

--- common/src/main/java/einstein/ambient_sleep/client/particle/FireflyParticle.java
+++ common/src/main/java/einstein/ambient_sleep/client/particle/FireflyParticle.java
@@ -1,23 +1,32 @@
 package einstein.ambient_sleep.client.particle;
 
-import einstein.ambient_sleep.util.LoopingSpriteSet;
 import net.minecraft.client.multiplayer.ClientLevel;
-import net.minecraft.client.particle.*;
+import net.minecraft.client.particle.Particle;
+import net.minecraft.client.particle.ParticleProvider;
+import net.minecraft.client.particle.SpriteSet;
+import net.minecraft.client.particle.SuspendedParticle;
+import net.minecraft.client.renderer.texture.TextureAtlasSprite;
 import net.minecraft.core.particles.SimpleParticleType;
 import net.minecraft.util.RandomSource;
 import org.jetbrains.annotations.Nullable;
 
 public class FireflyParticle extends SuspendedParticle {
 
-    private final LoopingSpriteSet sprites;
+    private static final int FIRST_FRAME_TICKS = 16;
+    private static final int TOTAL_FRAMES = 19; // Technically there are 20 but 0 is the first frame
+    private static final int TOTAL_ANIMATION_TICKS = TOTAL_FRAMES + FIRST_FRAME_TICKS;
+
+    private final SpriteSet sprites;
+    private int animationTick;
+    private int currentFrame;
 
     public FireflyParticle(ClientLevel level, SpriteSet sprites, double x, double y, double z, double xSpeed, double ySpeed, double zSpeed) {
         super(level, sprites, x, y, z, xSpeed, ySpeed, zSpeed);
-        this.sprites = (LoopingSpriteSet) sprites;
+        this.sprites = sprites;
         hasPhysics = true;
-        lifetime *= 2;
+        lifetime = 105; // will allow the animation to play 3
         setColor(1, 1, 1);
-        setSprite(this.sprites.ambientSleep$nextSpite());
+        setSpriteFrame();
     }
 
     @Override
@@ -29,7 +38,25 @@ public class FireflyParticle extends SuspendedParticle {
     public void tick() {
         super.tick();
         if (!removed) {
-            setSprite(sprites.ambientSleep$nextSpite());
+            animationTick++;
+            if (animationTick < FIRST_FRAME_TICKS) {
+                currentFrame = 0;
+            }
+            else if (animationTick < TOTAL_ANIMATION_TICKS) {
+                currentFrame++;
+            }
+            else {
+                currentFrame = 0;
+                animationTick = 0;
+            }
+            setSpriteFrame();
+        }
+    }
+
+    private void setSpriteFrame() {
+        if (!removed) {
+            TextureAtlasSprite sprite = sprites.get(currentFrame, TOTAL_FRAMES);
+            setSprite(sprite);
         }
     }
 
--- common/src/main/java/einstein/ambient_sleep/util/LoopingSpriteSet.java
+++
@@ -1,8 +0,0 @@
-package einstein.ambient_sleep.util;
-
-import net.minecraft.client.renderer.texture.TextureAtlasSprite;
-
-public interface LoopingSpriteSet {
-
-    TextureAtlasSprite ambientSleep$nextSpite();
-}
