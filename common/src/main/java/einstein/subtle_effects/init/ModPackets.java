9624c2f5ab0c18d7e03761657e2ca3cbd6a2003b
MincraftEinstein <70532030+MincraftEinstein@users.noreply.github.com>
2024-09-09 20:54:00 -0400

    Remove dummy packet

--- common/src/main/java/einstein/subtle_effects/init/ModPackets.java
+++ common/src/main/java/einstein/subtle_effects/init/ModPackets.java
@@ -1,20 +1,21 @@
 package einstein.subtle_effects.init;
 
-import einstein.subtle_effects.networking.clientbound.*;
+import einstein.subtle_effects.networking.clientbound.ClientBoundEntityFellPacket;
+import einstein.subtle_effects.networking.clientbound.ClientBoundEntitySpawnSprintingDustCloudsPacket;
+import einstein.subtle_effects.networking.clientbound.ClientBoundSpawnSnoreParticlePacket;
+import einstein.subtle_effects.networking.clientbound.ClientPacketHandlers;
 
 import static einstein.subtle_effects.platform.Services.NETWORK;
 
 public class ModPackets {
 
     public static void init() {
-        NETWORK.registerToClient(DummyPacket.TYPE, DummyPacket.STREAM_CODEC);
         NETWORK.registerToClient(ClientBoundSpawnSnoreParticlePacket.TYPE, ClientBoundSpawnSnoreParticlePacket.STREAM_CODEC);
         NETWORK.registerToClient(ClientBoundEntityFellPacket.TYPE, ClientBoundEntityFellPacket.STREAM_CODEC);
         NETWORK.registerToClient(ClientBoundEntitySpawnSprintingDustCloudsPacket.TYPE, ClientBoundEntitySpawnSprintingDustCloudsPacket.STREAM_CODEC);
     }
 
     public static void initClientHandlers() {
-        NETWORK.registerClientHandler(DummyPacket.TYPE, DummyPacket::handle);
         NETWORK.registerClientHandler(ClientBoundSpawnSnoreParticlePacket.TYPE, ClientPacketHandlers::handle);
         NETWORK.registerClientHandler(ClientBoundEntityFellPacket.TYPE, ClientPacketHandlers::handle);
         NETWORK.registerClientHandler(ClientBoundEntitySpawnSprintingDustCloudsPacket.TYPE, ClientPacketHandlers::handle);
