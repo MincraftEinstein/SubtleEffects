ebccb602d70f5d9098600fb20450f0ad8204de07
MincraftEinstein <70532030+MincraftEinstein@users.noreply.github.com>
2024-10-14 20:30:49 -0400

    Added register function to ModSounds

--- common/src/main/java/einstein/subtle_effects/init/ModSounds.java
+++ common/src/main/java/einstein/subtle_effects/init/ModSounds.java
@@ -7,12 +7,16 @@ import java.util.function.Supplier;
 
 public class ModSounds {
 
-    public static final Supplier<SoundEvent> VILLAGER_SNORE = Services.REGISTRY.registerSound("entity.villager.snore");
-    public static final Supplier<SoundEvent> PLAYER_SNORE = Services.REGISTRY.registerSound("entity.player.snore");
-    public static final Supplier<SoundEvent> PLAYER_STOMACH_GROWL = Services.REGISTRY.registerSound("entity.player.stomach_growl");
-    public static final Supplier<SoundEvent> PLAYER_HEARTBEAT = Services.REGISTRY.registerSound("entity.player.heartbeat");
-    public static final Supplier<SoundEvent> AMETHYST_CLUSTER_CHIME = Services.REGISTRY.registerSound("block.amethyst_cluster.chime");
+    public static final Supplier<SoundEvent> VILLAGER_SNORE = register("entity.villager.snore");
+    public static final Supplier<SoundEvent> PLAYER_SNORE = register("entity.player.snore");
+    public static final Supplier<SoundEvent> PLAYER_STOMACH_GROWL = register("entity.player.stomach_growl");
+    public static final Supplier<SoundEvent> PLAYER_HEARTBEAT = register("entity.player.heartbeat");
+    public static final Supplier<SoundEvent> AMETHYST_CLUSTER_CHIME = register("block.amethyst_cluster.chime");
 
     public static void init() {
+    }
+
+    private static Supplier<SoundEvent> register(String name) {
+        return Services.REGISTRY.registerSound(name);
     }
 }
