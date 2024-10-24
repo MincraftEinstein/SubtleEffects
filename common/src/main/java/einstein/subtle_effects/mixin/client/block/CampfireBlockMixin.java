e922c40195b65d0d5f1af857d28ce0cf82e94c39
MincraftEinstein <70532030+MincraftEinstein@users.noreply.github.com>
2024-10-24 17:16:01 -0400

    Replaced campfire food smoke with steam

--- common/src/main/java/einstein/subtle_effects/mixin/client/block/CampfireBlockMixin.java
+++ common/src/main/java/einstein/subtle_effects/mixin/client/block/CampfireBlockMixin.java
@@ -1,7 +1,11 @@
 package einstein.subtle_effects.mixin.client.block;
 
 import com.llamalad7.mixinextras.injector.ModifyExpressionValue;
+import com.llamalad7.mixinextras.injector.wrapoperation.Operation;
+import com.llamalad7.mixinextras.injector.wrapoperation.WrapOperation;
 import einstein.subtle_effects.init.ModConfigs;
+import einstein.subtle_effects.init.ModParticles;
+import net.minecraft.core.particles.SimpleParticleType;
 import net.minecraft.world.level.block.CampfireBlock;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
@@ -12,5 +16,13 @@ public class CampfireBlockMixin {
     @ModifyExpressionValue(method = "animateTick", at = @At(value = "FIELD", target = "Lnet/minecraft/world/level/block/CampfireBlock;spawnParticles:Z"))
     private boolean shouldDisableLavaSparks(boolean original) {
         return original && !ModConfigs.BLOCKS.sparks.removeVanillaCampfireSparks;
+    }
+
+    @WrapOperation(method = "makeParticles", at = @At(value = "FIELD", target = "Lnet/minecraft/core/particles/ParticleTypes;SMOKE:Lnet/minecraft/core/particles/SimpleParticleType;"))
+    private static SimpleParticleType replaceSmoke(Operation<SimpleParticleType> original) {
+        if (ModConfigs.BLOCKS.updatedSmoke.campfireDowseSmoke) {
+            return ModParticles.SMOKE.get();
+        }
+        return original.call();
     }
 }
