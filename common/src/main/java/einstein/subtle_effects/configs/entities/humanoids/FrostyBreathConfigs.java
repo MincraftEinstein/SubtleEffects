cbca5efba5787aea0f877040814e2fa9a7b30f01
MincraftEinstein <70532030+MincraftEinstein@users.noreply.github.com>
2024-12-07 23:46:24 -0500

    Added an additional biomes frosty breath config

--- common/src/main/java/einstein/subtle_effects/configs/entities/humanoids/FrostyBreathConfigs.java
+++ common/src/main/java/einstein/subtle_effects/configs/entities/humanoids/FrostyBreathConfigs.java
@@ -1,13 +1,16 @@
 package einstein.subtle_effects.configs.entities.humanoids;
 
+import einstein.subtle_effects.configs.ModBiomeConfigs;
 import einstein.subtle_effects.configs.ModEntityConfigs;
 import einstein.subtle_effects.init.ModConfigs;
 import me.fzzyhmstrs.fzzy_config.annotations.Translation;
 import me.fzzyhmstrs.fzzy_config.config.ConfigSection;
 import me.fzzyhmstrs.fzzy_config.util.EnumTranslatable;
+import me.fzzyhmstrs.fzzy_config.validation.collection.ValidatedList;
 import me.fzzyhmstrs.fzzy_config.validation.number.ValidatedFloat;
 import me.fzzyhmstrs.fzzy_config.validation.number.ValidatedInt;
 import me.fzzyhmstrs.fzzy_config.validation.number.ValidatedNumber;
+import net.minecraft.resources.ResourceLocation;
 import org.jetbrains.annotations.NotNull;
 
 @Translation(prefix = ModConfigs.BASE_KEY + "entities.humanoids.frostyBreath")
@@ -17,6 +20,7 @@ public class FrostyBreathConfigs extends ConfigSection {
     public ValidatedFloat alpha = new ValidatedFloat(0.5F, 1, 0.2F, ValidatedNumber.WidgetType.SLIDER);
     public ValidatedInt waitTime = new ValidatedInt(60, 200, 10);
     public Seasons seasons = Seasons.DEFAULT;
+    public ValidatedList<ResourceLocation> additionalBiomes = ModBiomeConfigs.biomeList();
 
     public enum Seasons implements EnumTranslatable {
         OFF,
