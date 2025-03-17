82813aee3b707f1d516c44aeeaba48f204e208cd
MincraftEinstein <70532030+MincraftEinstein@users.noreply.github.com>
2025-03-17 15:32:46 -0400

    Added wait time configs for heart beating and stomach growling

--- common/src/main/java/einstein/subtle_effects/util/Util.java
+++ common/src/main/java/einstein/subtle_effects/util/Util.java
@@ -33,7 +33,6 @@ public class Util {
     public static final int BREATH_DELAY = 60;
     public static final int SNORE_DELAY = 10;
     public static final int MAX_Z_COUNT = 3;
-    public static final int STOMACH_GROWL_DELAY = 300;
     public static final DustParticleOptions GLOWSTONE_DUST_PARTICLES = new DustParticleOptions(Vec3.fromRGB24(0xFFBC5E).toVector3f(), 1);
     public static final ResourceLocation COLORLESS_RAIN_TEXTURE = SubtleEffects.loc("textures/environment/colorless_rain.png");
     public static final Gson GSON = new GsonBuilder().create();
