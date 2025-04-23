bd7a9903d8bbde0da39e2686cfa3911bd7d95234
MincraftEinstein <70532030+MincraftEinstein@users.noreply.github.com>
2025-04-23 13:24:18 -0400

    Added night vision fading

--- common/src/main/java/einstein/subtle_effects/configs/ModGeneralConfigs.java
+++ common/src/main/java/einstein/subtle_effects/configs/ModGeneralConfigs.java
@@ -37,6 +37,7 @@ public class ModGeneralConfigs extends Config {
 
     public boolean mobSkullShaders = true;
     public ValidatedFloat fireHeight = new ValidatedFloat(-0.15F, 0.4F, -0.5F);
+    public boolean nightVisionFading = true;
 
     public ModGeneralConfigs() {
         super(SubtleEffects.loc("general"));
