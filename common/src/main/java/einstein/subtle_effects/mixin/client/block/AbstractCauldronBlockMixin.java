0b83d484154b6b262212d2a7aceedf3e1a12f5e8
MincraftEinstein <70532030+MincraftEinstein@users.noreply.github.com>
2025-06-08 00:23:22 -0400

    Actually used cauldron use configs

--- common/src/main/java/einstein/subtle_effects/mixin/client/block/AbstractCauldronBlockMixin.java
+++ common/src/main/java/einstein/subtle_effects/mixin/client/block/AbstractCauldronBlockMixin.java
@@ -18,6 +18,8 @@ import net.minecraft.world.level.block.state.BlockState;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 
+import static einstein.subtle_effects.init.ModConfigs.BLOCKS;
+
 @Mixin(AbstractCauldronBlock.class)
 public class AbstractCauldronBlockMixin {
 
@@ -26,28 +28,34 @@ public class AbstractCauldronBlockMixin {
         if (level.isClientSide && result == ItemInteractionResult.SUCCESS) {
             RandomSource random = level.getRandom();
 
-            TickerManager.schedule(2, () -> {
-                BlockState newState = level.getBlockState(pos);
-                boolean isEmpty = newState.is(Blocks.CAULDRON);
-                double fluidHeight = Util.getCauldronFillHeight(isEmpty ? state : newState);
-                ParticleOptions particle = Util.getCauldronParticle(isEmpty ? state : newState);
+            if (BLOCKS.cauldronUseParticles || BLOCKS.cauldronCleanItemSounds) {
+                TickerManager.schedule(2, () -> {
+                    if (BLOCKS.cauldronUseParticles) {
+                        BlockState newState = level.getBlockState(pos);
+                        boolean isEmpty = newState.is(Blocks.CAULDRON);
+                        double fluidHeight = Util.getCauldronFillHeight(isEmpty ? state : newState);
+                        ParticleOptions particle = Util.getCauldronParticle(isEmpty ? state : newState);
 
-                if (fluidHeight > 0 && particle != null) {
-                    for (int i = 0; i < 16; i++) {
-                        level.addParticle(
-                                particle,
-                                pos.getX() + random.nextDouble(),
-                                pos.getY() + fluidHeight,
-                                pos.getZ() + random.nextDouble(),
-                                0, 0, 0
-                        );
+                        if (fluidHeight > 0 && particle != null) {
+                            for (int i = 0; i < 16; i++) {
+                                level.addParticle(
+                                        particle,
+                                        pos.getX() + random.nextDouble(),
+                                        pos.getY() + fluidHeight,
+                                        pos.getZ() + random.nextDouble(),
+                                        0, 0, 0
+                                );
+                            }
+                        }
                     }
-                }
 
-                if (interaction.equals(CauldronInteraction.DYED_ITEM) || interaction.equals(CauldronInteraction.BANNER) || interaction.equals(CauldronInteraction.SHULKER_BOX)) {
-                    Util.playClientSound(pos, ModSounds.CAULDRON_CLEAN_ITEM.get(), SoundSource.BLOCKS, 1, (random.nextFloat() - random.nextFloat()) * 0.2F + 1);
-                }
-            });
+                    if (BLOCKS.cauldronCleanItemSounds) {
+                        if (interaction.equals(CauldronInteraction.DYED_ITEM) || interaction.equals(CauldronInteraction.BANNER) || interaction.equals(CauldronInteraction.SHULKER_BOX)) {
+                            Util.playClientSound(pos, ModSounds.CAULDRON_CLEAN_ITEM.get(), SoundSource.BLOCKS, 1, (random.nextFloat() - random.nextFloat()) * 0.2F + 1);
+                        }
+                    }
+                });
+            }
         }
         return result;
     }
