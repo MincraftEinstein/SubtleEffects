e69d80030329e0ab6efc42d3b2e67656d0678268
TheEnderCore <theendercore@gmail.com>
2025-10-14 00:02:37 +0300

    out with the optimizations for now

--- common/src/main/java/einstein/subtle_effects/mixin/client/particle/ParticleEngineMixin.java
+++ common/src/main/java/einstein/subtle_effects/mixin/client/particle/ParticleEngineMixin.java
@@ -1,34 +1,18 @@
 package einstein.subtle_effects.mixin.client.particle;
 
-import com.llamalad7.mixinextras.injector.v2.WrapWithCondition;
 import com.llamalad7.mixinextras.sugar.Local;
-import com.mojang.blaze3d.vertex.VertexConsumer;
 import einstein.subtle_effects.data.BCWPPackManager;
-import einstein.subtle_effects.init.ModSpriteSets;
-import einstein.subtle_effects.util.FrustumGetter;
-import einstein.subtle_effects.util.ParticleAccessor;
 import einstein.subtle_effects.util.Util;
-import net.minecraft.client.Camera;
-import net.minecraft.client.Minecraft;
 import net.minecraft.client.multiplayer.ClientLevel;
 import net.minecraft.client.particle.Particle;
 import net.minecraft.client.particle.ParticleEngine;
-import net.minecraft.client.renderer.culling.Frustum;
 import net.minecraft.core.BlockPos;
 import net.minecraft.core.particles.ParticleOptions;
-import net.minecraft.resources.ResourceLocation;
-import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
-import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
-
-import java.util.Map;
-
-import static einstein.subtle_effects.init.ModConfigs.GENERAL;
 
 @Mixin(ParticleEngine.class)
 public class ParticleEngineMixin {
@@ -52,7 +36,8 @@ public class ParticleEngineMixin {
         }
     }
 
-    @WrapWithCondition(method = "renderParticleType*", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/particle/Particle;render(Lcom/mojang/blaze3d/vertex/VertexConsumer;Lnet/minecraft/client/Camera;F)V"))
+    // TODO (ender) fix some of this later
+  /*  @WrapWithCondition(method = "renderParticleType*", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/particle/Particle;render(Lcom/mojang/blaze3d/vertex/VertexConsumer;Lnet/minecraft/client/Camera;F)V"))
     private static boolean shouldRenderParticle(Particle particle, VertexConsumer consumer, Camera camera, float partialTick) {
         return subtleEffects$shouldRenderParticle(particle, camera);
     }
@@ -76,5 +61,5 @@ public class ParticleEngineMixin {
             return accessor.subtleEffects$wasForced() || camera.getPosition().distanceToSqr(accessor.getX(), accessor.getY(), accessor.getZ()) < distance * distance;
         }
         return false;
-    }
+    }*/
 }
