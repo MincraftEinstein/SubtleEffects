f1e7948e78a2e0435435dd801c5aa0e7fe845e34
MincraftEinstein <70532030+MincraftEinstein@users.noreply.github.com>
2025-10-21 14:01:08 -0400

    Improved blended particle render type | Fixes #151

--- common/src/main/java/einstein/subtle_effects/init/ModParticleRenderTypes.java
+++ common/src/main/java/einstein/subtle_effects/init/ModParticleRenderTypes.java
@@ -2,30 +2,44 @@ package einstein.subtle_effects.init;
 
 import com.mojang.blaze3d.platform.GlStateManager;
 import com.mojang.blaze3d.systems.RenderSystem;
+import com.mojang.blaze3d.vertex.BufferBuilder;
 import com.mojang.blaze3d.vertex.DefaultVertexFormat;
+import com.mojang.blaze3d.vertex.Tesselator;
 import com.mojang.blaze3d.vertex.VertexFormat;
 import einstein.subtle_effects.mixin.client.particle.ParticleEngineAccessor;
 import net.minecraft.client.particle.ParticleRenderType;
 import net.minecraft.client.renderer.texture.TextureAtlas;
+import net.minecraft.client.renderer.texture.TextureManager;
+import org.jetbrains.annotations.NotNull;
 
 import java.util.ArrayList;
 import java.util.List;
 
 public class ModParticleRenderTypes {
 
-    public static final ParticleRenderType BLENDED = (tesselator, textureManager) -> {
-        RenderSystem.depthMask(false);
+    public static final ParticleRenderType BLENDED = new ParticleRenderType() {
 
-        // noinspection deprecation
-        RenderSystem.setShaderTexture(0, TextureAtlas.LOCATION_PARTICLES);
-        RenderSystem.enableBlend();
-        RenderSystem.blendFunc(GlStateManager.SourceFactor.SRC_ALPHA, GlStateManager.DestFactor.ONE_MINUS_SRC_ALPHA);
-        return tesselator.begin(VertexFormat.Mode.QUADS, DefaultVertexFormat.PARTICLE);
+        @Override
+        public BufferBuilder begin(Tesselator tesselator, @NotNull TextureManager textureManager) {
+            RenderSystem.depthMask(false);
+
+            // noinspection deprecation
+            RenderSystem.setShaderTexture(0, TextureAtlas.LOCATION_PARTICLES);
+            RenderSystem.enableBlend();
+            RenderSystem.blendFunc(GlStateManager.SourceFactor.SRC_ALPHA, GlStateManager.DestFactor.ONE_MINUS_SRC_ALPHA);
+            return tesselator.begin(VertexFormat.Mode.QUADS, DefaultVertexFormat.PARTICLE);
+        }
+
+        @Override
+        public String toString() {
+            return "subtle_effects:blended";
+        }
     };
 
     public static void init() {
         List<ParticleRenderType> renderTypes = new ArrayList<>(ParticleEngineAccessor.getRenderOrder());
-        renderTypes.add(BLENDED);
+        int i = renderTypes.indexOf(ParticleRenderType.PARTICLE_SHEET_TRANSLUCENT);
+        renderTypes.add(++i, BLENDED);
         ParticleEngineAccessor.setRenderOrder(List.copyOf(renderTypes));
     }
 
