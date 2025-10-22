e5bfb3f31c286791d061744140ca0d77c23c14f0
TheEnderCore <theendercore@gmail.com>
2025-10-23 00:07:57 +0300

    Temp FlatPlaneParticle

--- common/src/main/java/einstein/subtle_effects/particle/FlatPlaneParticle.java
+++ common/src/main/java/einstein/subtle_effects/particle/FlatPlaneParticle.java
@@ -27,29 +27,29 @@ public abstract class FlatPlaneParticle extends SingleQuadParticle {
     }
 
     @Override
-    public void render(VertexConsumer consumer, Camera camera, float partialTicks) {
+    public void extract(QuadParticleRenderState state, Camera camera, float partialTicks) {
         Quaternionf rotation = new Quaternionf(this.rotation);
         if (roll != 0) {
             rotation.rotateZ(Mth.lerp(partialTicks, oRoll, roll));
         }
 
-        renderQuad(consumer, camera, partialTicks, rotation);
+        renderQuad(state, camera, partialTicks, rotation);
     }
 
-    protected void renderQuad(VertexConsumer consumer, Camera camera, float partialTicks, Quaternionf rotation) {
+    protected void renderQuad(QuadParticleRenderState state, Camera camera, float partialTicks, Quaternionf rotation) {
         Vec3 vec3 = camera.getPosition();
         float x = (float) (Mth.lerp(partialTicks, xo, this.x) - vec3.x());
         float y = (float) (Mth.lerp(partialTicks, yo, this.y) - vec3.y());
         float z = (float) (Mth.lerp(partialTicks, zo, this.z) - vec3.z());
 
-        renderQuad(consumer, rotation, partialTicks, x, y, z, false);
+        renderQuad(state, rotation, partialTicks, x, y, z, false);
 
         if (renderBackFace) {
-            renderQuad(consumer, rotation, partialTicks, x, y, z, true);
+            renderQuad(state, rotation, partialTicks, x, y, z, true);
         }
     }
 
-    protected void renderQuad(VertexConsumer consumer, Quaternionf rotation, float partialTicks, float x, float y, float z, boolean renderInverted) {
+    protected void renderQuad(QuadParticleRenderState state, Quaternionf rotation, float partialTicks, float x, float y, float z, boolean renderInverted) {
         float quadSize = getQuadSize(partialTicks);
         float u0 = getU0();
         float u1 = getU1();
@@ -58,11 +58,19 @@ public abstract class FlatPlaneParticle extends SingleQuadParticle {
         int packedLight = getLightColor(partialTicks);
         int i = renderInverted ? 1 : -1;
         int i2 = renderInverted ? -1 : 1;
-
-        renderVertex(consumer, rotation, x, y, z, i, -1, quadSize, u1, v1, packedLight);
-        renderVertex(consumer, rotation, x, y, z, i, 1, quadSize, u1, v0, packedLight);
-        renderVertex(consumer, rotation, x, y, z, i2, 1, quadSize, u0, v0, packedLight);
-        renderVertex(consumer, rotation, x, y, z, i2, -1, quadSize, u0, v1, packedLight);
+        state.add(
+                getLayer(),
+                x, y, z,
+                rotation.x, rotation.y, rotation.z, rotation.w,
+                getQuadSize(partialTicks),
+                getU0(), getU1(), getV0(), getV1(),
+                ARGB.colorFromFloat(alpha, rCol, gCol, bCol),
+                getLightColor(partialTicks)
+        );
+//        renderVertex(state, rotation, x, y, z, i, -1, quadSize, u1, v1, packedLight);
+//        renderVertex(state, rotation, x, y, z, i, 1, quadSize, u1, v0, packedLight);
+//        renderVertex(state, rotation, x, y, z, i2, 1, quadSize, u0, v0, packedLight);
+//        renderVertex(state, rotation, x, y, z, i2, -1, quadSize, u0, v1, packedLight);
     }
 
     protected void renderVertex(VertexConsumer buffer, Quaternionf rotation, float x, float y, float z, float xOffset, float yOffset, float quadSize, float u, float v, int packedLight) {
