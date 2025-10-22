6d74e652b9bea7df4572ad74c773b679a6cb1a2f
TheEnderCore <theendercore@gmail.com>
2025-10-23 00:48:16 +0300

    ScreenEffectRendererMixin

--- common/src/main/java/einstein/subtle_effects/mixin/client/ScreenEffectRendererMixin.java
+++ common/src/main/java/einstein/subtle_effects/mixin/client/ScreenEffectRendererMixin.java
@@ -1,12 +1,11 @@
 package einstein.subtle_effects.mixin.client;
 
-import com.llamalad7.mixinextras.injector.v2.WrapWithCondition;
 import com.mojang.blaze3d.vertex.PoseStack;
 import einstein.subtle_effects.init.ModConfigs;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.renderer.MultiBufferSource;
 import net.minecraft.client.renderer.ScreenEffectRenderer;
-import net.minecraft.world.effect.MobEffects;
+import net.minecraft.client.renderer.texture.TextureAtlasSprite;
 import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
@@ -21,13 +20,12 @@ public class ScreenEffectRendererMixin {
     @Final
     private Minecraft minecraft;
 
-    @WrapWithCondition(method = "renderScreenEffect", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/renderer/ScreenEffectRenderer;renderFire(Lcom/mojang/blaze3d/vertex/PoseStack;Lnet/minecraft/client/renderer/MultiBufferSource;)V"))
-    private boolean shouldRenderFire(PoseStack poseStack, MultiBufferSource bufferSource) {
-        return !ModConfigs.GENERAL.fireResistanceDisablesFireRendering || !minecraft.player.hasEffect(MobEffects.FIRE_RESISTANCE);
-    }
+//    private boolean shouldRenderFire(PoseStack poseStack, MultiBufferSource bufferSource) {
+//        return !ModConfigs.GENERAL.fireResistanceDisablesFireRendering || !minecraft.player.hasEffect(MobEffects.FIRE_RESISTANCE);
+//    }
 
     @Inject(method = "renderFire", at = @At(value = "INVOKE", target = "Lcom/mojang/blaze3d/vertex/PoseStack;translate(FFF)V"))
-    private static void adjustFireHeight(PoseStack poseStack, MultiBufferSource source, CallbackInfo ci) {
+    private static void adjustFireHeight(PoseStack poseStack, MultiBufferSource bufferSource, TextureAtlasSprite sprite, CallbackInfo ci) {
         poseStack.translate(0, ModConfigs.GENERAL.fireHeight.get(), 0);
     }
 }
