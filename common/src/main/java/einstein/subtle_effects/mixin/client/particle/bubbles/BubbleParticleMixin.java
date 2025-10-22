17aef2779d15479633bf10bae8127325f2ccad56
TheEnderCore <theendercore@gmail.com>
2025-10-23 00:04:41 +0300

    BubbleParticleMixin

--- common/src/main/java/einstein/subtle_effects/mixin/client/particle/bubbles/BubbleParticleMixin.java
+++ common/src/main/java/einstein/subtle_effects/mixin/client/particle/bubbles/BubbleParticleMixin.java
@@ -1,6 +1,5 @@
 package einstein.subtle_effects.mixin.client.particle.bubbles;
 
-import com.mojang.blaze3d.vertex.VertexConsumer;
 import einstein.subtle_effects.data.BCWPPackManager;
 import einstein.subtle_effects.init.ModConfigs;
 import einstein.subtle_effects.init.ModParticles;
@@ -9,14 +8,15 @@ import einstein.subtle_effects.platform.Services;
 import einstein.subtle_effects.util.BubbleSetter;
 import net.minecraft.client.multiplayer.ClientLevel;
 import net.minecraft.client.particle.*;
+import net.minecraft.client.renderer.state.QuadParticleRenderState;
 import net.minecraft.client.renderer.texture.TextureAtlasSprite;
 import net.minecraft.core.BlockPos;
 import net.minecraft.core.particles.ParticleTypes;
 import net.minecraft.sounds.SoundEvents;
 import net.minecraft.sounds.SoundSource;
+import net.minecraft.util.ARGB;
 import net.minecraft.util.Mth;
 import org.joml.Quaternionf;
-import org.joml.Vector3f;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
@@ -70,34 +70,30 @@ public abstract class BubbleParticleMixin extends SingleQuadParticle implements 
     }
 
     @Override
-    protected void renderRotatedQuad(VertexConsumer buffer, Quaternionf quaternion, float x, float y, float z, float partialTicks) {
-        super.renderRotatedQuad(buffer, quaternion, x, y, z, partialTicks);
+    protected void extractRotatedQuad(QuadParticleRenderState state, Quaternionf quaternion, float x, float y, float z, float partialTicks) {
+        super.extractRotatedQuad(state, quaternion, x, y, z, partialTicks);
         if (subtleEffects$overlaySprite != null) {
-            int lightColor = getLightColor(partialTicks);
-            float quadSize = getQuadSize(partialTicks);
             float u0 = subtleEffects$overlaySprite.getU0();
             float u1 = subtleEffects$overlaySprite.getU1();
             float v0 = subtleEffects$overlaySprite.getV0();
             float v1 = subtleEffects$overlaySprite.getV1();
-
-            subtleEffects$renderVertex(buffer, quaternion, x, y, z, 1, -1, quadSize, u1, v1, lightColor);
-            subtleEffects$renderVertex(buffer, quaternion, x, y, z, 1, 1, quadSize, u1, v0, lightColor);
-            subtleEffects$renderVertex(buffer, quaternion, x, y, z, -1, 1, quadSize, u0, v0, lightColor);
-            subtleEffects$renderVertex(buffer, quaternion, x, y, z, -1, -1, quadSize, u0, v1, lightColor);
+            state.add(
+                    getLayer(), x, y, z,
+                    quaternion.x, quaternion.y, quaternion.z, quaternion.w,
+                    getQuadSize(partialTicks), u0, u1, v0, v1,
+                    subtleEffects$color(), getLightColor(partialTicks)
+            );
         }
     }
 
     @Unique
-    private void subtleEffects$renderVertex(VertexConsumer buffer, Quaternionf quaternion, float x, float y, float z, float xOffset, float yOffset, float quadSize, float u, float v, int packedLight) {
-        Vector3f vector3f = (new Vector3f(xOffset, yOffset, 0)).rotate(quaternion).mul(quadSize).add(x, y, z);
+    private int subtleEffects$color() {
         float colorIntensity = 0.20F;
         float whiteIntensity = 1 - colorIntensity;
 
-        buffer.addVertex(vector3f.x(), vector3f.y(), vector3f.z()).setUv(u, v)
-                .setColor(whiteIntensity + (colorIntensity * ((subtleEffects$waterColor >> 16 & 255) / 255F)),
-                        whiteIntensity + (colorIntensity * ((subtleEffects$waterColor >> 8 & 255) / 255F)),
-                        whiteIntensity + (colorIntensity * ((subtleEffects$waterColor & 255) / 255F)), alpha)
-                .setLight(packedLight);
+        return ARGB.colorFromFloat(whiteIntensity + (colorIntensity * ((subtleEffects$waterColor >> 16 & 255) / 255F)),
+                whiteIntensity + (colorIntensity * ((subtleEffects$waterColor >> 8 & 255) / 255F)),
+                whiteIntensity + (colorIntensity * ((subtleEffects$waterColor & 255) / 255F)), alpha);
     }
 
     @Override
