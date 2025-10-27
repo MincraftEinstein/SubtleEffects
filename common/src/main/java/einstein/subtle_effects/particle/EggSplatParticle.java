5a78eaeb78ef51ee536a26b385609336602a3dd7
TheEnderCore <theendercore@gmail.com>
2025-10-27 22:34:50 +0200

    EggSplatParticle

--- common/src/main/java/einstein/subtle_effects/particle/EggSplatParticle.java
+++ common/src/main/java/einstein/subtle_effects/particle/EggSplatParticle.java
@@ -1,6 +1,5 @@
 package einstein.subtle_effects.particle;
 
-import com.mojang.blaze3d.vertex.VertexConsumer;
 import einstein.subtle_effects.particle.option.DirectionParticleOptions;
 import einstein.subtle_effects.util.Util;
 import net.minecraft.client.multiplayer.ClientLevel;
@@ -12,8 +11,8 @@ import net.minecraft.core.BlockPos;
 import net.minecraft.core.Direction;
 import net.minecraft.util.Mth;
 import net.minecraft.util.RandomSource;
-import org.joml.Quaternionf;
-import org.joml.Vector3f;
+
+import static einstein.subtle_effects.util.Util.radians;
 
 public class EggSplatParticle extends FlatPlaneParticle {
 
@@ -22,12 +21,12 @@ public class EggSplatParticle extends FlatPlaneParticle {
     protected EggSplatParticle(ClientLevel level, double x, double y, double z, Direction direction, TextureAtlasSprite sprite) {
         super(level, x, y, z, sprite);
         this.direction = direction;
-        rotation = direction.getRotation().rotateX(180 * Mth.DEG_TO_RAD);
+        rotation = direction.getRotation().rotateX(radians(-90));
         lifetime = 120;
         quadSize = 0.2F;
 
         if (direction.getAxis().isVertical()) {
-            rotation.rotateY(90 * random.nextInt(3) * Mth.DEG_TO_RAD);
+            rotation.rotateZ(radians(90 * random.nextInt(3)));
         }
     }
 
@@ -56,12 +55,6 @@ public class EggSplatParticle extends FlatPlaneParticle {
         if (age >= i) {
             alpha = Mth.clamp(alpha - 0.0125F, 0, 1);
         }
-    }
-
-    @Override
-    protected void renderVertex(VertexConsumer buffer, Quaternionf rotation, float x, float y, float z, float xOffset, float yOffset, float quadSize, float u, float v, int packedLight) {
-        Vector3f vector3f = new Vector3f(xOffset, 0, yOffset).rotate(rotation).mul(quadSize).add(x, y, z);
-        buffer.addVertex(vector3f.x(), vector3f.y(), vector3f.z()).setUv(u, v).setColor(rCol, gCol, bCol, alpha).setLight(packedLight);
     }
 
     @Override
