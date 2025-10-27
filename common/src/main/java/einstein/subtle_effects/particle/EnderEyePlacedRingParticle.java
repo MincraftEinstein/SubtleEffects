6da2d542b14be59433f8195ae728e8bf8093db84
TheEnderCore <theendercore@gmail.com>
2025-10-27 23:05:45 +0200

    EnderEyePlacedRingParticle

--- common/src/main/java/einstein/subtle_effects/particle/EnderEyePlacedRingParticle.java
+++ common/src/main/java/einstein/subtle_effects/particle/EnderEyePlacedRingParticle.java
@@ -1,6 +1,5 @@
 package einstein.subtle_effects.particle;
 
-import com.mojang.blaze3d.vertex.VertexConsumer;
 import einstein.subtle_effects.init.ModConfigs;
 import einstein.subtle_effects.init.ModParticleLayers;
 import net.minecraft.client.Camera;
@@ -8,12 +7,15 @@ import net.minecraft.client.multiplayer.ClientLevel;
 import net.minecraft.client.particle.Particle;
 import net.minecraft.client.particle.ParticleProvider;
 import net.minecraft.client.particle.SpriteSet;
+import net.minecraft.client.renderer.state.QuadParticleRenderState;
 import net.minecraft.client.renderer.texture.TextureAtlasSprite;
 import net.minecraft.core.particles.ColorParticleOption;
 import net.minecraft.util.Mth;
 import net.minecraft.util.RandomSource;
 import net.minecraft.world.phys.Vec3;
 import org.joml.Quaternionf;
+
+import static einstein.subtle_effects.util.Util.radians;
 
 public class EnderEyePlacedRingParticle extends FlatPlaneParticle {
 
@@ -29,24 +31,24 @@ public class EnderEyePlacedRingParticle extends FlatPlaneParticle {
         alpha = lifetimeAlpha.startAlpha();
     }
 
-   /* @Override
-    protected void renderQuad(VertexConsumer consumer, Camera camera, float partialTicks, Quaternionf rotation) {
+    @Override
+    protected void renderQuad(QuadParticleRenderState state, Camera camera, float partialTicks, Quaternionf rotation) {
         alpha = lifetimeAlpha.currentAlphaForAge(age, lifetime, partialTicks);
         Vec3 cameraPos = camera.getPosition();
         float x = (float) (Mth.lerp(partialTicks, xo, this.x) - cameraPos.x());
         float y = (float) (Mth.lerp(partialTicks, yo, this.y) - cameraPos.y());
         float z = (float) (Mth.lerp(partialTicks, zo, this.z) - cameraPos.z());
 
-        renderQuad(consumer, new Quaternionf(), partialTicks, x, y, z + SIZE);
-        renderQuad(consumer, new Quaternionf().rotateY(90 * Mth.DEG_TO_RAD), partialTicks, x - SIZE, y, z);
-        renderQuad(consumer, new Quaternionf().rotateY(180 * Mth.DEG_TO_RAD), partialTicks, x, y, z - SIZE);
-        renderQuad(consumer, new Quaternionf().rotateY(-90 * Mth.DEG_TO_RAD), partialTicks, x + SIZE, y, z);
+        renderQuad(state, new Quaternionf(), partialTicks, x, y, z + SIZE);
+        renderQuad(state, new Quaternionf().rotateY(radians(90)), partialTicks, x - SIZE, y, z);
+        renderQuad(state, new Quaternionf().rotateY(radians(180)), partialTicks, x, y, z - SIZE);
+        renderQuad(state, new Quaternionf().rotateY(radians(-90)), partialTicks, x + SIZE, y, z);
     }
 
-    protected void renderQuad(VertexConsumer consumer, Quaternionf rotation, float partialTicks, float x, float y, float z) {
-        renderQuad(consumer, rotation, partialTicks, x, y, z, false);
-        renderQuad(consumer, rotation, partialTicks, x, y, z, true);
-    }*/
+    protected void renderQuad(QuadParticleRenderState state, Quaternionf rotation, float partialTicks, float x, float y, float z) {
+        renderQuad(state, rotation, partialTicks, x, y, z, false);
+        renderQuad(state, rotation, partialTicks, x, y, z, true);
+    }
 
     @Override
     protected int getLightColor(float partialTick) {
