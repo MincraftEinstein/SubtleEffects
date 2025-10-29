dbfe629774b3a0864b1542f95d1a0a79cca9b8f3
MincraftEinstein <70532030+MincraftEinstein@users.noreply.github.com>
2025-10-29 17:53:35 -0400

    Fixed crash caused by feeding mooshrooms the last item in a stack | Fixes #164

--- common/src/main/java/einstein/subtle_effects/mixin/client/entity/MooshroomMixin.java
+++ common/src/main/java/einstein/subtle_effects/mixin/client/entity/MooshroomMixin.java
@@ -3,7 +3,6 @@ package einstein.subtle_effects.mixin.client.entity;
 import com.llamalad7.mixinextras.injector.wrapoperation.Operation;
 import com.llamalad7.mixinextras.injector.wrapoperation.WrapOperation;
 import com.llamalad7.mixinextras.sugar.Local;
-import einstein.subtle_effects.init.ModConfigs;
 import einstein.subtle_effects.util.ParticleSpawnUtil;
 import einstein.subtle_effects.util.Util;
 import net.minecraft.core.particles.ColorParticleOption;
@@ -11,7 +10,10 @@ import net.minecraft.core.particles.ItemParticleOption;
 import net.minecraft.core.particles.ParticleOptions;
 import net.minecraft.core.particles.ParticleTypes;
 import net.minecraft.util.RandomSource;
+import net.minecraft.world.InteractionHand;
+import net.minecraft.world.InteractionResult;
 import net.minecraft.world.entity.animal.MushroomCow;
+import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.component.SuspiciousStewEffects;
 import net.minecraft.world.level.Level;
@@ -19,9 +21,13 @@ import net.minecraft.world.phys.Vec3;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 import java.util.List;
 import java.util.Optional;
+
+import static einstein.subtle_effects.init.ModConfigs.ENTITIES;
 
 @Mixin(MushroomCow.class)
 public class MooshroomMixin {
@@ -31,7 +37,7 @@ public class MooshroomMixin {
 
     @WrapOperation(method = "mobInteract", at = @At(value = "INVOKE", target = "Lnet/minecraft/world/level/Level;addParticle(Lnet/minecraft/core/particles/ParticleOptions;DDDDDD)V", ordinal = 0))
     private void spawnFeedingFailedParticles(Level level, ParticleOptions particle, double x, double y, double z, double xSpeed, double ySpeed, double zSpeed, Operation<Void> original) {
-        if (ModConfigs.ENTITIES.improvedBrownMooshroomFeedingEffects) {
+        if (ENTITIES.improvedBrownMooshroomFeedingEffects) {
             for (int i = 0; i < 5; i++) {
                 level.addParticle(particle,
                         subtleEffects$me.getRandomX(1),
@@ -45,33 +51,41 @@ public class MooshroomMixin {
         original.call(level, particle, x, y, z, xSpeed, ySpeed, zSpeed);
     }
 
-    @WrapOperation(method = "mobInteract", at = @At(value = "INVOKE", target = "Lnet/minecraft/world/level/Level;addParticle(Lnet/minecraft/core/particles/ParticleOptions;DDDDDD)V", ordinal = 1))
-    private void spawnFeedingParticles(Level level, ParticleOptions particle, double x, double y, double z, double xSpeed, double ySpeed, double zSpeed, Operation<Void> original, @Local Optional<SuspiciousStewEffects> optional, @Local(ordinal = 0) ItemStack heldStack) {
+    @Inject(method = "mobInteract", at = @At(value = "INVOKE", target = "Lnet/minecraft/world/item/ItemStack;consume(ILnet/minecraft/world/entity/LivingEntity;)V"))
+    private void spawnFeedingParticles(Player player, InteractionHand hand, CallbackInfoReturnable<InteractionResult> cir, @Local Optional<SuspiciousStewEffects> optional, @Local(ordinal = 0) ItemStack heldStack) {
+        Level level = subtleEffects$me.level();
         RandomSource random = subtleEffects$me.getRandom();
 
-        if (ModConfigs.ENTITIES.animalFeedingParticles) {
-            for (int i = 0; i < 4; i++) {
-                ParticleSpawnUtil.spawnEntityFaceParticle(new ItemParticleOption(ParticleTypes.ITEM, heldStack),
-                        subtleEffects$me, level, random, new Vec3(0, 0.3, -0.2), Util.getPartialTicks()
+        if (ENTITIES.animalFeedingParticles && !heldStack.isEmpty()) {
+            ItemParticleOption options = new ItemParticleOption(ParticleTypes.ITEM, heldStack);
+
+            for (int i = 0; i < 16; i++) {
+                ParticleSpawnUtil.spawnEntityFaceParticle(options, subtleEffects$me, level,
+                        random, new Vec3(0, 0.3, -0.2), Util.getPartialTicks()
                 );
             }
         }
 
-        if (ModConfigs.ENTITIES.improvedBrownMooshroomFeedingEffects) {
+        if (ENTITIES.improvedBrownMooshroomFeedingEffects) {
             List<SuspiciousStewEffects.Entry> effects = optional.get().effects();
             int color = effects.get(random.nextInt(effects.size())).effect().value().getColor();
+            ColorParticleOption colorOptions = ColorParticleOption.create(ParticleTypes.ENTITY_EFFECT, color);
 
-            for (int i = 0; i < 2; i++) {
-                level.addParticle(ColorParticleOption.create(ParticleTypes.ENTITY_EFFECT, color),
+            for (int i = 0; i < 8; i++) {
+                level.addParticle(colorOptions,
                         subtleEffects$me.getRandomX(1),
                         subtleEffects$me.getRandomY(),
                         subtleEffects$me.getRandomZ(1),
                         0, 0, 0
                 );
             }
-            return;
         }
+    }
 
-        original.call(level, particle, x, y, z, xSpeed, ySpeed, zSpeed);
+    @WrapOperation(method = "mobInteract", at = @At(value = "INVOKE", target = "Lnet/minecraft/world/level/Level;addParticle(Lnet/minecraft/core/particles/ParticleOptions;DDDDDD)V", ordinal = 1))
+    private void spawnEffectParticles(Level level, ParticleOptions particle, double x, double y, double z, double xSpeed, double ySpeed, double zSpeed, Operation<Void> original) {
+        if (!ENTITIES.improvedBrownMooshroomFeedingEffects) {
+            original.call(level, particle, x, y, z, xSpeed, ySpeed, zSpeed);
+        }
     }
 }
