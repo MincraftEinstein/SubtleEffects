c19203c7d9d6131fb3746c3c1d5dac163603c181
TheEnderCore <theendercore@gmail.com>
2026-03-21 01:58:45 +0200

    withBlend was killed today

--- common/src/main/java/einstein/subtle_effects/init/ModPipelines.java
+++ common/src/main/java/einstein/subtle_effects/init/ModPipelines.java
@@ -1,7 +1,10 @@
 package einstein.subtle_effects.init;
 
 import com.mojang.blaze3d.pipeline.BlendFunction;
+import com.mojang.blaze3d.pipeline.ColorTargetState;
+import com.mojang.blaze3d.pipeline.DepthStencilState;
 import com.mojang.blaze3d.pipeline.RenderPipeline;
+import com.mojang.blaze3d.platform.CompareOp;
 import com.mojang.blaze3d.platform.DestFactor;
 import com.mojang.blaze3d.platform.SourceFactor;
 import net.minecraft.client.renderer.RenderPipelines;
@@ -16,15 +19,15 @@ public class ModPipelines {
     public static final RenderPipeline CUSTOM_TRANSLUCENT_PARTICLE_PIPELINE = RenderPipelines.register(
             RenderPipeline.builder(RenderPipelines.PARTICLE_SNIPPET)
                     .withLocation(loc("pipeline/entity_particle_translucent"))
-                    .withBlend(BlendFunction.TRANSLUCENT)
+                    .withColorTargetState(new ColorTargetState(BlendFunction.TRANSLUCENT))
                     .withCull(false)
                     .build());
 
     public static final RenderPipeline BLENDED_PIPELINE = RenderPipelines.register(
             RenderPipeline.builder(RenderPipelines.PARTICLE_SNIPPET)
                     .withLocation(loc("pipeline/alpha_blend"))
-                    .withBlend(ALPHA_BLEND)
-                    .withDepthWrite(false)
+                    .withColorTargetState(new ColorTargetState(ALPHA_BLEND))
+                    .withDepthStencilState(new DepthStencilState(CompareOp.ALWAYS_PASS, false))
                     .build()
     );
 
