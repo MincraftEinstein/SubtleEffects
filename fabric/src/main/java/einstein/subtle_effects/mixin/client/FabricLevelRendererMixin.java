8cb2094e41c983d7bcdadebd72fbe16dd08f42ce
TheEnderCore <theendercore@gmail.com>
2026-03-23 22:03:39 +0200

    fixed FabricLevelRendererMixin

--- fabric/src/main/java/einstein/subtle_effects/mixin/client/FabricLevelRendererMixin.java
+++ fabric/src/main/java/einstein/subtle_effects/mixin/client/FabricLevelRendererMixin.java
@@ -3,15 +3,16 @@ package einstein.subtle_effects.mixin.client;
 import com.llamalad7.mixinextras.sugar.Local;
 import com.mojang.blaze3d.buffers.GpuBufferSlice;
 import com.mojang.blaze3d.resource.GraphicsResourceAllocator;
-import com.mojang.blaze3d.resource.ResourceHandle;
 import com.mojang.blaze3d.vertex.PoseStack;
 import einstein.subtle_effects.client.renderer.ParticleBoundingBoxesRenderer;
 import net.minecraft.client.Camera;
 import net.minecraft.client.DeltaTracker;
 import net.minecraft.client.renderer.LevelRenderer;
+import net.minecraft.client.renderer.chunk.ChunkSectionsToRender;
 import net.minecraft.client.renderer.culling.Frustum;
+import net.minecraft.client.renderer.state.level.CameraRenderState;
 import net.minecraft.client.renderer.state.level.LevelRenderState;
-import org.joml.Matrix4f;
+import org.joml.Matrix4fc;
 import org.joml.Vector4f;
 import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
@@ -27,13 +28,13 @@ public class FabricLevelRendererMixin {
     @Final
     private LevelRenderState levelRenderState;
 
-    @Inject(method = "renderLevel", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/renderer/WorldBorderRenderer;extract(Lnet/minecraft/world/level/border/WorldBorder;FLnet/minecraft/world/phys/Vec3;DLnet/minecraft/client/renderer/state/WorldBorderRenderState;)V", shift = At.Shift.AFTER))
-    private void extractParticleBoundingBoxes(GraphicsResourceAllocator graphicsResourceAllocator, DeltaTracker deltaTracker, boolean renderBlockOutline, Camera camera, Matrix4f frustumMatrix, Matrix4f projectionMatrix, Matrix4f cullingProjectionMatrix, GpuBufferSlice shaderFog, Vector4f fogColor, boolean renderSky, CallbackInfo ci, @Local Frustum frustum) {
+    @Inject(method = "extractLevel", at = @At(value = "INVOKE", target = "Lnet/minecraft/util/profiling/ProfilerFiller;popPush(Ljava/lang/String;)V", ordinal = 10))
+    private void extractParticleBoundingBoxes(DeltaTracker deltaTracker, Camera camera, float deltaPartialTick, CallbackInfo ci, @Local(name = "cullFrustum") Frustum frustum) {
         ParticleBoundingBoxesRenderer.extractParticleBoundingBoxes(levelRenderState, camera, frustum);
     }
 
-    @Inject(method = "method_62213", at = @At(value = "TAIL"))
-    private void renderParticleBoundingBoxes(GpuBufferSlice gpuBufferSlice, ResourceHandle<?> mainResourceHandle, ResourceHandle<?> particlesResourceHandle, CallbackInfo ci) {
+    @Inject(method = "renderLevel", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/renderer/LevelRenderer;addLateDebugPass(Lcom/mojang/blaze3d/framegraph/FrameGraphBuilder;Lnet/minecraft/client/renderer/state/level/CameraRenderState;Lcom/mojang/blaze3d/buffers/GpuBufferSlice;Lorg/joml/Matrix4fc;)V"))
+    private void renderParticleBoundingBoxes(GraphicsResourceAllocator resourceAllocator, DeltaTracker deltaTracker, boolean renderOutline, CameraRenderState cameraState, Matrix4fc modelViewMatrix, GpuBufferSlice terrainFog, Vector4f fogColor, boolean shouldRenderSky, ChunkSectionsToRender chunkSectionsToRender, CallbackInfo ci) {
         ParticleBoundingBoxesRenderer.renderParticleBoundingBoxes(new PoseStack(), levelRenderState);
     }
 }
