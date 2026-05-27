2b68096d87b27da411897bb7ca2e3d73ef4180af
TheEnderCore <theendercore@gmail.com>
2026-05-27 16:48:17 +0300

    Fixed Slime particles

--- common/src/main/java/einstein/subtle_effects/mixin/client/entity/SlimeMixin.java
+++ common/src/main/java/einstein/subtle_effects/mixin/client/entity/SlimeMixin.java
@@ -1,7 +1,6 @@
 package einstein.subtle_effects.mixin.client.entity;
 
-import com.llamalad7.mixinextras.injector.wrapoperation.Operation;
-import com.llamalad7.mixinextras.injector.wrapoperation.WrapOperation;
+import com.llamalad7.mixinextras.injector.ModifyReturnValue;
 import einstein.subtle_effects.init.ModConfigs;
 import net.minecraft.core.particles.BlockParticleOption;
 import net.minecraft.core.particles.ParticleOptions;
@@ -14,11 +13,11 @@ import org.spongepowered.asm.mixin.injection.At;
 @Mixin(Slime.class)
 public class SlimeMixin {
 
-    @WrapOperation(method = "tick", at = @At(value = "INVOKE", target = "Lnet/minecraft/world/entity/monster/Slime;getParticleType()Lnet/minecraft/core/particles/ParticleOptions;"))
-    private ParticleOptions getParticleType(Slime entity, Operation<ParticleOptions> original) {
-        if (ModConfigs.ENTITIES.replaceSlimeSquishParticles && entity.getType().equals(EntityType.SLIME)) {
+    @ModifyReturnValue(method = "getParticleType", at = @At("RETURN"))
+    private ParticleOptions getParticleType(ParticleOptions original) {
+        if (ModConfigs.ENTITIES.replaceSlimeSquishParticles) {
             return new BlockParticleOption(ParticleTypes.BLOCK, Blocks.SLIME_BLOCK.defaultBlockState());
         }
-        return original.call(entity);
+        return original;
     }
 }
