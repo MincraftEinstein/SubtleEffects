05c5f69669719704395288f708466dfab63de86d
MincraftEinstein <70532030+MincraftEinstein@users.noreply.github.com>
2026-09-08 16:09:20 -0400

    Potential/partial fix for lava splashes sometimes crashing due to missing textures | Closes #219 #237 #242

--- common/src/main/java/einstein/subtle_effects/data/SpriteSetHolder.java
+++ common/src/main/java/einstein/subtle_effects/data/SpriteSetHolder.java
@@ -1,21 +1,15 @@
 package einstein.subtle_effects.data;
 
 import net.minecraft.client.particle.ParticleEngine;
-import net.minecraft.resources.ResourceLocation;
 import org.jetbrains.annotations.Nullable;
 
 import java.util.function.Supplier;
 
 public class SpriteSetHolder implements Supplier<ParticleEngine.MutableSpriteSet> {
 
-    private final ResourceLocation id;
     @Nullable
     private ParticleEngine.MutableSpriteSet spriteSet;
     private boolean referencesPreExisting = false;
-
-    public SpriteSetHolder(ResourceLocation id) {
-        this.id = id;
-    }
 
     public void set(ParticleEngine.MutableSpriteSet spriteSet, boolean referencesPreExisting) {
         this.spriteSet = spriteSet;
@@ -25,7 +19,7 @@ public class SpriteSetHolder implements Supplier<ParticleEngine.MutableSpriteSet
     @Override
     public ParticleEngine.MutableSpriteSet get() {
         if (spriteSet == null) {
-            throw new IllegalStateException("No sprite set is set for sprite set holder '" + id + "'");
+            spriteSet = MissingSpriteSet.INSTANCE;
         }
 
         return spriteSet;
--- fabric/src/main/java/einstein/subtle_effects/mixin/client/particle/FabricParticleEngineMixin.java
+++ fabric/src/main/java/einstein/subtle_effects/mixin/client/particle/FabricParticleEngineMixin.java
@@ -1,7 +1,12 @@
 package einstein.subtle_effects.mixin.client.particle;
 
+import com.llamalad7.mixinextras.sugar.Local;
 import einstein.subtle_effects.data.DynamicSpriteSetsManager;
+import einstein.subtle_effects.data.MissingSpriteSet;
+import net.minecraft.client.multiplayer.ClientLevel;
 import net.minecraft.client.particle.ParticleEngine;
+import net.minecraft.client.renderer.texture.TextureAtlasSprite;
+import net.minecraft.client.renderer.texture.TextureManager;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.server.packs.resources.Resource;
 import org.spongepowered.asm.mixin.Final;
@@ -9,6 +14,7 @@ import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 import java.util.Map;
@@ -22,8 +28,19 @@ public abstract class FabricParticleEngineMixin {
     @Final
     private Map<ResourceLocation, ParticleEngine.MutableSpriteSet> spriteSets;
 
+    @Inject(method = "<init>", at = @At("TAIL"))
+    private void init(ClientLevel level, TextureManager textureManager, CallbackInfo ci) {
+        spriteSets.put(MissingSpriteSet.ID, MissingSpriteSet.INSTANCE);
+    }
+
     @Inject(method = "method_45772", at = @At("HEAD"))
-    private void allowDynamicSpriteSetLoading(Executor executor, Map<ResourceLocation, Resource> resources, CallbackInfoReturnable<CompletionStage<?>> cir) {
-        DynamicSpriteSetsManager.reload(spriteSets);
+    private void beginReloadingDynamicSpriteSets(Executor executor, Map<ResourceLocation, Resource> resources, CallbackInfoReturnable<CompletionStage<?>> cir) {
+        DynamicSpriteSetsManager.beginReload(spriteSets);
+    }
+
+    @Inject(method = "method_45766", at = @At(value = "INVOKE", target = "Ljava/util/List;forEach(Ljava/util/function/Consumer;)V"))
+    private void finishReloadingDynamicSpriteSets(CallbackInfo ci, @Local TextureAtlasSprite missingSprite) {
+        MissingSpriteSet.INSTANCE.rebind(missingSprite);
+        DynamicSpriteSetsManager.finishReload(spriteSets);
     }
 }
