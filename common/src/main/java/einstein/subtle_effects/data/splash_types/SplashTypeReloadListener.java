8fbd20d312e02e9cea3db017e60cd0b09d067939
MincraftEinstein <70532030+MincraftEinstein@users.noreply.github.com>
2026-09-21 20:58:09 -0400

    Improvements to dynamic sprite set loading

--- common/src/main/java/einstein/subtle_effects/data/splash_types/SplashTypeReloadListener.java
+++ common/src/main/java/einstein/subtle_effects/data/splash_types/SplashTypeReloadListener.java
@@ -26,8 +26,9 @@ public class SplashTypeReloadListener extends SimplePreparableReloadListener<Map
 
     private static final String DIRECTORY = "subtle_effects/splash_types";
     public static final Map<ResourceLocation, SplashType> SPLASH_TYPES = new HashMap<>();
+    public static boolean HAS_PREPARED = false;
 
-    // TODO need to fix sometimes being called after the dynamic sprite set reloading has begun
+    // TODO possibly is sometimes loaded after dynamic sprite sets have begun loading
     @Override
     protected Map<ResourceLocation, SplashType> prepare(ResourceManager resourceManager, ProfilerFiller profiler) {
         Map<ResourceLocation, JsonElement> resources = new HashMap<>();
@@ -40,11 +41,13 @@ public class SplashTypeReloadListener extends SimplePreparableReloadListener<Map
                         .ifPresent(typeData -> load(id, typeData, splashTypes))
         );
         LOGGER.info("Loaded {} splash types", splashTypes.size());
+        HAS_PREPARED = true;
         return splashTypes;
     }
 
     @Override
     protected void apply(Map<ResourceLocation, SplashType> resources, ResourceManager manager, ProfilerFiller profiler) {
+        HAS_PREPARED = false;
         SPLASH_TYPES.clear();
         SPLASH_TYPES.putAll(resources);
     }
